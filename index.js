b005504647e49a30388459257f6a73546df094c3
Aharper9917 <allen.harper9917@gmail.com>
2024-04-11 11:21:07 -0600

    Add cooldowns

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const { token } = require('./config.json').discord;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.cooldowns = new Collection();
 
 // COMMANDS
 client.commands = new Collection();
