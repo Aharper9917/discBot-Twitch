9d02d4d1d43bb74896d081f37d6979fe8e6f6b66
Aharper9917 <allen.harper9917@gmail.com>
2024-04-11 16:50:44 -0600

    BING BONG

--- commands/utils/bing.js
+++ commands/utils/bing.js
@@ -1,11 +1,11 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 const data = new SlashCommandBuilder()
-  .setName('ping')
+  .setName('bing')
   .setDescription('Replies with Pong!');
 
 const execute = async (interaction) => {
-  await interaction.reply('Pong!');
+  await interaction.reply('bong!');
 }
 
 module.exports = {
