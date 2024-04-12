4d927bfc4c230900b44eba6b753f84bca39e45de
Aharper9917 <allen.harper9917@gmail.com>
2024-04-12 12:08:59 -0600

    Fix Imports

--- commands/twitch/set-channel.js
+++ commands/twitch/set-channel.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, ChannelType, PermissionFlagsBits } = require('discord.js');
 
 const data = new SlashCommandBuilder()
   .setName('set-channel')
