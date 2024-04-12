fac9e185fb324d1a3b0292b55c75478f7439b9fd
Aharper9917 <allen.harper9917@gmail.com>
2024-04-11 23:24:15 -0600

    test

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
 // require('dotenv').config()
+console.log(process.env)
 require('module-alias/register')
 const fs = require('node:fs');
 const path = require('node:path');
