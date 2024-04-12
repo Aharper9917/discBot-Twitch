62e2746a6bdede30f2b772752e63ba191c766694
Aharper9917 <allen.harper9917@gmail.com>
2024-04-12 13:44:39 -0600

    Update docker volume for data

--- db/database.js
+++ db/database.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 const db = new Sequelize('database', 'username', 'password', {
   host: 'localhost',
   dialect: 'sqlite',
-  storage: 'data/database.sqlite',
+  storage: '../data/database.sqlite',
   logging: false,
 });
 
