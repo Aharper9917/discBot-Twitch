a161bd82bc204851a78aaf7de0c9e07403cbc9b1
Aharper9917 <allen.harper9917@gmail.com>
2024-04-12 13:49:47 -0600

    Update db logging and storage path

--- db/database.js
+++ db/database.js
@@ -3,8 +3,8 @@ const Sequelize = require('sequelize');
 const db = new Sequelize('database', 'username', 'password', {
   host: 'localhost',
   dialect: 'sqlite',
-  storage: '../data/database.sqlite',
-  logging: false,
+  storage: 'data/database.sqlite',
+  logging: true,
 });
 
 module.exports = db
