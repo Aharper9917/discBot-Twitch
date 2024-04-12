26fb80fe63a3276afa6e759420e7f5571b411bda
Aharper9917 <allen.harper9917@gmail.com>
2024-04-12 13:59:24 -0600

    disable db logging

--- db/database.js
+++ db/database.js
@@ -4,7 +4,7 @@ const db = new Sequelize('database', 'username', 'password', {
   host: 'localhost',
   dialect: 'sqlite',
   storage: 'data/database.sqlite',
-  logging: true,
+  logging: false,
 });
 
 module.exports = db
