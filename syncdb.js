42e5fc06311a194e6f405f06d89cad39d9d926d8
Aharper9917 <allen.harper9917@gmail.com>
2024-04-12 20:05:25 -0600

    Update syncdb && package.json scripts

--- syncdb.js
+++ syncdb.js
@@ -1,14 +1,15 @@
 require('module-alias/register')
-const db = require('@db/database')
 const Guild = require('@db/models/guild')
 const Notification = require('@db/models/notification')
 
 try {
+  const isForce = !!process.argv.slice(2)[0]
+  const options = { alter: true, force: isForce }
+  console.log('options', options)
   console.log(`Started syncing Database Models.`)
-  
-  Guild.sync({force: true})
-  Notification.sync({force: true})
-  db.sync({ force: true })
+
+  Guild.sync(options)
+  Notification.sync(options)
   
   console.log(`Successfully Synced Database Models.`)  
 } catch (error) {
