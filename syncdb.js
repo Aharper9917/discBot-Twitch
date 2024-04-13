a12937c5ba024ef9f4a343bed3ab924940278251
Aharper9917 <allen.harper9917@gmail.com>
2024-04-12 20:37:39 -0600

    Added different sync scripts

--- syncdb.js
+++ syncdb.js
@@ -1,10 +1,12 @@
 require('module-alias/register')
+require('@db/relations')
 const Guild = require('@db/models/guild')
 const Notification = require('@db/models/notification')
 
 try {
-  const isForce = !!process.argv.slice(2)[0]
-  const options = { alter: true, force: isForce }
+  const force = !!process.argv.slice(2).includes('force')
+  const alter = !!process.argv.slice(2).includes('alter')
+  const options = { alter: alter, force: force }
   console.log('options', options)
   console.log(`Started syncing Database Models.`)
 
