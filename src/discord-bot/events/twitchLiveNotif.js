30573f8eaadb97faba0b4c980f3bab5d6d804477
Aharper9917 <allen.harper9917@gmail.com>
2024-04-16 22:15:52 -0600

    format channel message for twitch-live-notification

--- src/discord-bot/events/twitchLiveNotif.js
+++ src/discord-bot/events/twitchLiveNotif.js
@@ -1,5 +1,20 @@
+/* {
+  "id": "50897254045",
+  "broadcaster_user_id": "105252332",
+  "broadcaster_user_login": "slaysol",
+  "broadcaster_user_name": "Slaysol",
+  "type": "live",
+  "started_at": "2024-04-17T03:24:58Z"
+} */
+
 const execute = async (client) => {
-  console.log(`DISCORD RECIEVED NOTIF ALERT`, client.liveEvent);
+  const [ dbGuild, dbCreated ] = await Guild.findOrCreate({ where: { id: client.guild.id } })
+  const dbNotif = await Notification.findOne({ where: {
+    guildId: client.guild.id,
+    twitchUsername: client.liveEvent.broadcaster_user_login
+  }})
+
+  client.channels.cache.get(dbGuild.notificationChannelId).send(`<@everyone> [${dbNotif.twitchUsername}](${dbNotif.twitchUrl}) just went live on Twitch!\n\n`);
 }
 
 module.exports = {
