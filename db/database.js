2b22f95f0ba7d3bb8cf4a8509674aece70c47913
Allen <53236834+Aharper9917@users.noreply.github.com>
2025-07-23 07:08:38 +0000

    fix mariadb

--- db/database.js
+++ db/database.js
@@ -1,27 +1,23 @@
 const Sequelize = require("sequelize");
 
-let db;
+// const db = new Sequelize("database", "username", "password", {
+//   host: "localhost",
+//   dialect: "sqlite",
+//   storage: "data/database.sqlite",
+//   logging: false,
+// });
 
-if (process.env.DATABASE_TYPE == "sqlite") {
-  db = new Sequelize("database", "username", "password", {
-    host: "localhost",
-    dialect: "sqlite",
-    storage: "data/database.sqlite",
-    logging: false,
-  });
-} else if (process.env.DATABASE_TYPE == "mariadb") {
-  db = new Sequelize("database", "username", "password", {
-    dialect: "mariadb",
-    dialectOptions: {
-      database: process.env.DATABASE,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      showWarnings: true,
-      connectTimeout: 1000,
-    },
-  });
-}
+const db = new Sequelize("database", "username", "password", {
+  dialect: "mariadb",
+  dialectOptions: {
+    database: process.env.DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    showWarnings: true,
+    connectTimeout: 1000,
+  },
+});
 
 module.exports = db;
