bb5cd9fc476e043d0aea0fe4563c9a57793c3006
allen <allen.harper9917@gmail.com>
2025-07-22 23:10:55 -0600

    Integrate MariaDB

--- db/database.js
+++ db/database.js
@@ -1,10 +1,26 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const db = new Sequelize('database', 'username', 'password', {
-  host: 'localhost',
-  dialect: 'sqlite',
-  storage: 'data/database.sqlite',
+const sqlitedb = new Sequelize("database", "username", "password", {
+  host: "localhost",
+  dialect: "sqlite",
+  storage: "data/database.sqlite",
   logging: false,
 });
 
-module.exports = db
+const mariadb = new Sequelize({
+  dialect: MariaDbDialect,
+  database: process.env.DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  showWarnings: true,
+  connectTimeout: 1000,
+});
+
+module.exports =
+  process.env.DATABASE_TYPE == "mariadb"
+    ? mariadb
+    : process.env.DATABASE_TYPE == "sqlite"
+    ? sqlitedb
+    : null;
