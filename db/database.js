55ac226d740b0926ea1d2150cbf48aec32c87625
Allen <53236834+Aharper9917@users.noreply.github.com>
2025-07-23 07:19:34 +0000

    fix mariadb

--- db/database.js
+++ db/database.js
@@ -18,4 +18,15 @@ const db = new Sequelize(
   }
 );
 
+async function testConnection() {
+    try {
+        await db.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+testConnection();
+
 module.exports = db;
