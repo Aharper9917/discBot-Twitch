3ef582e907b2cedac6b01171c85803916eb4ab30
Allen <53236834+Aharper9917@users.noreply.github.com>
2025-07-23 07:02:58 +0000

    fix mariadb

--- db/database.js
+++ db/database.js
@@ -1,26 +1,27 @@
 const Sequelize = require("sequelize");
 
-const sqlitedb = new Sequelize("database", "username", "password", {
-  host: "localhost",
-  dialect: "sqlite",
-  storage: "data/database.sqlite",
-  logging: false,
-});
+let db;
 
-const mariadb = new Sequelize({
-  dialect: MariaDbDialect,
-  database: process.env.DATABASE,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  showWarnings: true,
-  connectTimeout: 1000,
-});
+if (process.env.DATABASE_TYPE == "sqlite") {
+  db = new Sequelize("database", "username", "password", {
+    host: "localhost",
+    dialect: "sqlite",
+    storage: "data/database.sqlite",
+    logging: false,
+  });
+} else if (process.env.DATABASE_TYPE == "mariadb") {
+  db = new Sequelize("database", "username", "password", {
+    dialect: "mariadb",
+    dialectOptions: {
+      database: process.env.DATABASE,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      showWarnings: true,
+      connectTimeout: 1000,
+    },
+  });
+}
 
-module.exports =
-  process.env.DATABASE_TYPE == "mariadb"
-    ? mariadb
-    : process.env.DATABASE_TYPE == "sqlite"
-    ? sqlitedb
-    : null;
+module.exports = db;
